c85e76c5f3b76aafad655ad8d0289d0c804eb26a
Philip Roth <50246817+rothberry@users.noreply.github.com>
2020-09-23 15:58:17 -0400

    checker logic done

--- emailChecker.js
+++ emailChecker.js
@@ -7,21 +7,9 @@
  * Specifically: Gmail will ignore the placement of "." in the username. And it will ignore any portion of the username after a "+".
  * test.email@gmail.com == test.email+spam@gmail.com == testemail@gmail.com
  */
-
-const emailChecker2 = emails => {
-  let uniqueEmails = []
-  for (let i = 0; i < emails.length; i++) {
-    let email = emailTransform(emails[i])
-    if (!uniqueEmails.includes(email)) {
-      uniqueEmails.push(email)
-    }
-  }
-  return uniqueEmails.length
-}
+const { test1, test2, test3, test4 } = require("./emailData")
 const emailChecker = (emails) => {
-  // Create an object with key of username and value of domain?
-  // Create an object with key of domain and value of username?
-
+  // Create an object with key of username and value of domain
   let emailObj = {}
   emails.forEach((email) => {
     // First separate into halves at '@'
@@ -30,14 +18,21 @@ const emailChecker = (emails) => {
     // console.log(username, domain)
     username = makeUsername(username)
     // If there is no username key, then insert into emailObj
+    // if the username exists, then check if the array contains the domain
     if (!emailObj[username]) {
       emailObj[username] = [domain]
-    } else if (emailObj[username] !== domain) {
-      emailObj[username].push(domain)
+    } else {
+      if (!emailObj[username].includes(domain)) {
+        emailObj[username].push(domain)
+      }
     }
-    console.log(emailObj)
   })
-  return emailObj
+  // arr => [].concat(...arr)
+  const values = Object.values(emailObj) 
+  // const flattened = values => [].concat(...values)
+  const flattened =  values.reduce((acc, val) => acc.concat(val), []);
+  console.log(flattened)
+  return flattened.length
 }
 
 // Takes in the first half of the address and removes all extra characters
@@ -49,26 +44,13 @@ const makeUsername = (username) => {
     .split("+")[0]
 }
 
-const test1 = [
-  "test.email@gmail.com",
-  "test.email+spam@gmail.com",
-  "testemail@gmail.com",
-]
+// TESTING
+const res1 = emailChecker(test1)
+const res2 = emailChecker(test2)
+const res3 = emailChecker(test3)
+const res4 = emailChecker(test4)
 
-const test2 = [
-  "test.email@gmail.com",
-  "test.email@fetchrewards.com",
-  "test.email@hotmail.com",
-]
-
-const test3 = [
-  "test.email@gmail.com",
-  "test.email+spam+wowza+@gmail.com",
-  "t.e.s.t.e.m.a.i.l@gmail.com",
-  "t.e.s.t.e.m.a.i.l+spam0000000000@gmail.com",
-]
-
-// console.log(makeUsername('t.e.s.t+u.suckk'))
-console.log(emailChecker(test1))
-console.log(emailChecker(test2))
-console.log(emailChecker(test3))
+console.log(`\n1:`, "result:", res1, "\texpected:", 1, "\t", res1 === 1)
+console.log(`\n2:`, "result:", res2, "\texpected:", 3, "\t", res2 === 3)
+console.log(`\n3:`, "result:", res3, "\texpected:", 1, "\t", res3 === 1)
+console.log(`\n4:`, "result:", res4, "\texpected:", 15, "\t", res4 === 15)
--- emailData.js
+++ emailData.js
@@ -17,4 +17,27 @@ const test3 = [
   "t.e.s.t.e.m.a.i.l+spam0000000000@gmail.com",
 ]
 
-module.exports = {test1, test2, test3}
+const test4 = [
+  "test.email@gmail.com",
+  "test.eamail@gmail.com",
+  "test.emcaail@hail.com",
+  "test.emaascil@gail.com",
+  "test.emaail@gmaail.com",
+  "test.email@gmafil.com",
+  "test.email@gmaeil.com",
+  "test.emaail@gmail.com",
+  "test.emvail@gmail.com",
+  "test.email@gmail.com",
+  "test.emaail@gmail.com",
+  "tesat.email@gmaeil.com",
+  "test...email@gmail.com",
+  "test.em.ail@gmaivl.com",
+  "tes.t.email@gmavil.com",
+  "test..em.ail@gmawil.com",
+  "test..e.mail@gmawil.com",
+  "teswt.e.mail@gmail.com",
+  "test.em.ail@gmail.com",
+  "test.e.m+ail@gmail.com",
+]
+
+module.exports =  { test1, test2, test3 , test4}
